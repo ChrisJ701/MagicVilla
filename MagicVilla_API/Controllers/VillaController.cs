6740a4ed34129d13884c809122e82761288a1813
Christian Landaverde <christianjlandaverde@gmail.com>
2023-06-15 20:15:37 -0600

    agregados endpoints listar y obtener por id

---
+++ MagicVilla_API/Controllers/VillaController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MagicVilla_API.Modelos;
+using MagicVilla_API.Modelos.Dto;
+using MagicVilla_API.Datos;
+
+namespace MagicVilla_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<VillaDto>> GetVillas()
+        {
+            
+            return Ok(VillaStore.villaList);
+        
+        }
+
+        [HttpGet("id")]//endpoint que devuelve un objeto que coincida con el parámetro id que se le especifique
+        public ActionResult<VillaDto> GetVilla(int id)
+        {
+
+            if(id == 0) 
+            { 
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(v => v.Id == id);
+
+            if(villa == null)
+            {
+                return NotFound();//regresa un estado 404
+            }
+
+            return Ok(villa);
+
+        }
+    }
+}
---
+++ MagicVilla_API/Datos/VillaStore.cs
@@ -0,0 +1,16 @@
+﻿using MagicVilla_API.Modelos.Dto;
+
+
+namespace MagicVilla_API.Datos
+    //esta clase simula objetos almacenados como en una base de datos 
+    //para probar inicialmente el proyecto sin necesidad de crear base de datos
+{
+    public static class VillaStore
+    {
+        public static List<VillaDto> villaList = new List<VillaDto>
+        {
+            new VillaDto{Id=1, Nombre="Vista a la piscina"},
+            new VillaDto{Id=2, Nombre="Vista a la playa"}
+        };
+    }
+}
